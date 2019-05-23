cc44698777ddc18ad0a0f81b7c3d7d104bdce778
cilezez <istevanovic213@gmail.com>
2019-05-23 20:04:28 +0200

    script js

---
+++ script.js
@@ -0,0 +1,62 @@
+
+//##################
+//FUNCTIONS
+//##################
+
+//name-argument.mo<wmo ih imati koliko god hocemo kao i parametara.Prvi argument 
+//ce dobiti vrednost prvog parametra itd u niz.
+// function printName(text, name) {
+// 	console.log(text + name)
+// }
+//kad se prebacuje vrednost naziva se parametar.
+// printName("His name is ", "Peter");
+// printName("His name was ", "Mike");
+
+
+// var printName = function(age) {
+// 	console.log("His name is Peter")
+// };
+
+// printName(34);
+
+
+// var calculateAge = function(currentYear, yearOfBirth) {
+// 	return currentYear - yearOfBirth
+// };
+
+
+// var calculateYearOfBirth = function(currentYear, age)
+// 	return currentYear - age;
+
+
+
+// console.log(calculateYearOfBirth(2019,56))
+
+
+
+// var calculateAge = function(currentYear, yearOfBirth) {
+// 	return currentYear - yearOfBirth;
+// }
+
+// var printAge = function(cAge) {
+// 	var current = 2019;
+// 	var birthYear = 1962;
+
+// 	return cAge(current, birthYear);
+// }
+
+// console.log(printAge(calculateAge))
+
+
+// function combineNumbers (age) {
+// 	return function () {
+// 		console.log(age)
+// 	}
+// }
+
+// combineNumbers(23)()
+
+// (function(){ //funkcija koja zove sama sebe
+// 	console.log("Something")
+	
+// })();
